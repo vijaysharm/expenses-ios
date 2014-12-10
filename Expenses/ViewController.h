ee01433188b33482b7a9bc2827f177c03e0c5e26
Vijay Sharma <vijay.sharm@gmail.com>
2014-12-09 19:20:47 -0500

    Initial Commit

---
+++ Expenses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Expenses
+//
+//  Created by Vijay Sharma on 2014-12-09.
+//  Copyright (c) 2014 Vijay Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Expenses/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Expenses
+//
+//  Created by Vijay Sharma on 2014-12-09.
+//  Copyright (c) 2014 Vijay Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
